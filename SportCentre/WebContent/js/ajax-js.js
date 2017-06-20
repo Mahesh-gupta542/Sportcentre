bea4848556a0168d67a2cc9af3bed537a5ac18ca
Mahesh Gupta <mkg502@mun.ca>
2017-06-20 10:40:00 -0230

    Add files via upload

--- SportCentre/WebContent/js/ajax-js.js
+++ SportCentre/WebContent/js/ajax-js.js
@@ -6,16 +6,28 @@ $(document).ready(function(){
 	        	data : {
 	        		selectedValue : "Cricket"
 				},
+				dataType: 'json',
 	        	success: function(result){
-/*	        		var _html='<table >';
+	        		var _html='<table >';
 	        	    _html += '<tr>';
-	        	    _html += '<th>Blockk No</th>';
-	        	    _html += '<th>Lott No</th>';
-	        	    _html += '<th>Extentt</th>';
-	        	    _html += '<th>Land Value</th>';
-	        	    _html += '<th>On Booking Amount</th>';
-	        	    _html += '</tr>';*/
-	            $("#div1").html(result);
-	        }});
+	        	    _html += '<th>Match</th>';
+	        	    _html += '<th>Score</th>';
+	        	    _html += '</tr>';
+	        	    console.log(JSON.stringify(result.data));
+ 	        		$.each(result.data, function (index, value) {
+	        			_html += '<tr>';
+	        			_html += '<td>'+value.title+'</td>';
+	        			_html += '<td>'+value.score+'</td>';
+	        			_html += '</tr>';
+	        		});
+	        		_html += '</table>';
+	            $("#div1").html(_html);
+	        },
+	        error : function (jqXHR, status, err) {
+	            alert("Local error callback.");
+	          } 
+				
+	        });
+	        
 	    });
 	});
